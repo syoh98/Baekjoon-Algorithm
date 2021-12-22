72304f738e490a72793160ecfebb540b85a20c5f
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-22 18:59:08 +0900

    Create 5_A+B.java

---
+++ 1_chapter/5_A+B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		sum = a + b;
+		System.out.println(sum);
+
+	}
+}
