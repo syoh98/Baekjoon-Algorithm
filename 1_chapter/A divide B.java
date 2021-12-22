195279ff77802902fa0cd764b2a319a155a61328
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:07:57 +0900

    Create A divide B.java

---
+++ 1_chapter/A divide B.java
@@ -0,0 +1,19 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		DecimalFormat form = new DecimalFormat("#.################################");
+		
+		double sum = 0;
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		sum = (double)a / (double)b;
+		
+		System.out.println(form.format(sum));
+
+	}
+}
