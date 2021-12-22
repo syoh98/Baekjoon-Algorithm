0e4e615a69978645c46facbe78667481f90fca55
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:10:27 +0900

    Create 11_곱셈.java

---
+++ 1_chapter/11_곱셈.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		String b = scanner.next();
+		
+		scanner.close();
+		
+		System.out.println(a * (b.charAt(2) - '0' ));
+		System.out.println(a * (b.charAt(1) - '0'));
+		System.out.println(a * (b.charAt(0) - '0'));
+		System.out.println(a * Integer.parseInt(b));
+	}
+}
