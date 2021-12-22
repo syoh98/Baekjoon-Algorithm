f9de51dacf9630e0d52ee4f084247f76f2740511
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:13:07 +0900

    Create 3_윤년.java

---
+++ 2_chapter/3_윤년.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+
+		int year = in.nextInt();
+		// int B = in.nextInt();
+
+		in.close();
+
+		// 윤년일 경우
+		if ((int) year % 4 == 0) {
+			// 100의 배수가 아닐 때 또는 400의 배수일 때
+			if ((int) year % 100 != 0 || (int) year % 400 == 0) {
+				System.out.println("1");
+			} else {
+				System.out.println("0");
+			}
+		}
+		// 윤년이 아닐 경우
+		else {
+			System.out.println("0");
+		}
+	}
+}
