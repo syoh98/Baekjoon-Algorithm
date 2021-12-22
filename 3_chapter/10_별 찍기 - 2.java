be2f1869dd032638f2672c771054b2066c0b5552
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:22:09 +0900

    Create 10_별 찍기 - 2.java

---
+++ 3_chapter/10_별 찍기 - 2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	@SuppressWarnings("null")
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		StringBuilder sb = new StringBuilder();
+		String[] arr = new String[n];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = n; j >= 1; j--) {
+				if (i < j) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*"); // 공백 갯수
+				}
+			}
+			System.out.println();
+		}
+		scanner.close();
+	}
+}
