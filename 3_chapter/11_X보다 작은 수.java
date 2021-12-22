a256436c14872664eaa7fa0349c7f1d72fc0b839
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:22:42 +0900

    Create 11_X보다 작은 수.java

---
+++ 3_chapter/11_X보다 작은 수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	@SuppressWarnings("null")
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt(); // 입력받는 수의 갯수
+		int X = scanner.nextInt(); // X보다 작은수
+
+		int[] arr = new int[N+1];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		for (int j = 1; j <= N; j++) {
+			if (arr[j] < X) {
+				System.out.print(arr[j] + " ");
+			}
+		}
+		
+		scanner.close();
+	}
+}
