465f08e879f4f74d3fdda0462308f558fe96ffaf
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:15:33 +0900

    Create 구구단.java

---
+++ 3_chapter/구구단.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+
+		/*
+		 * int hour = in.nextInt(); int minute = in.nextInt(); int ahour = 0;
+		 */
+
+		int n = in.nextInt();
+
+		in.close();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(n + " * " + i + " = " + n * i);
+		}
+
+	}
+}
