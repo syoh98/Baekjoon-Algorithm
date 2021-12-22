5ac6a758de06e1c7bcfecb0894c430dfd9f6d450
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:13:39 +0900

    Create 4_사분면 고르기.java

---
+++ 2_chapter/4_사분면 고르기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+
+		int A = in.nextInt();
+		int B = in.nextInt();
+
+		in.close();
+
+		if (A > 0 && B > 0) {
+			System.out.println("1");
+		} else if (A < 0 && B > 0) {
+			System.out.println("2");
+		} else if (A < 0 && B < 0) {
+			System.out.println("3");
+		} else if (A > 0 && B < 0) {
+			System.out.println("4");
+		}
+	}
+}
