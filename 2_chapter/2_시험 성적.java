47119ddaa40897743e634ed55cfb6ace98be77ef
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-23 00:12:31 +0900

    Create 2_시험 성적.java

---
+++ 2_chapter/2_시험 성적.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+
+		int A = in.nextInt();
+		// int B = in.nextInt();
+
+		in.close();
+
+		if (90 <= A && A <= 100) {
+			System.out.println("A");
+		} else if (80 <= A && A < 90) {
+			System.out.println("B");
+		} else if (70 <= A && A < 80) {
+			System.out.println("C");
+		} else if (60 <= A && A < 70) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
