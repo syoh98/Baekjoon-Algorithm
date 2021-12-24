ed36136bc40293968a13e72d4fd344e0c5740564
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-25 00:53:31 +0900

    Create 2_최댓값.java

---
+++ 5_capter/2_최댓값.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] array = new int[10];
+		int max = 0;
+		int index = 0;
+
+		for (int i = 0; i <= 8; i++) {
+			array[i] = scanner.nextInt();
+		}
+		
+		max = 0;
+
+		for (int i = 0; i <= 8; i++) {
+			if (max < array[i]) {
+				max = array[i];
+				index = i + 1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(index);
+
+		scanner.close();
+	}
+}
