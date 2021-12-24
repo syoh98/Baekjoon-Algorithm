77937fb4d04aef75462c9910bbdbb6ddf4e00112
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-24 23:45:45 +0900

    Create A+B - 5.java

---
+++ 4_chapter/A+B - 5.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	@SuppressWarnings("null")
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		StringBuilder sb = new StringBuilder();
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		sb.append((Integer.toString(a + b)) + "\n");
+
+		while (a != 0 && b != 0) {
+			a = scanner.nextInt();
+			b = scanner.nextInt();
+
+			if(a == 0 && b == 0) {
+				break;
+			}
+			sb.append((Integer.toString(a + b)) + "\n");
+		}
+		System.out.print(sb);
+	}
+}
