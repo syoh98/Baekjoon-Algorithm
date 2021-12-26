8523317d947a906d0a16673a03def17bc6ee992e
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-26 23:32:07 +0900

    Create 1_아스키 코드.java

---
+++ 7_chapter/1_아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		char ch = scanner.nextLine().charAt(0); // 소문자 or 대문자 or 숫자
+		
+		System.out.println((int)ch);
+		
+		scanner.close();
+	}
+}
