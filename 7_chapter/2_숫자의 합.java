7f0f4c3f2593620bc65aae9a01271ea40b81cbb3
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-26 23:32:40 +0900

    Create 2_숫자의 합.java

---
+++ 7_chapter/2_숫자의 합.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		int sum = 0;
+		int N = scanner.nextInt();
+		String str = scanner.next();
+
+		scanner.close();
+
+		for (int i = 0; i < N; i++) {
+			sum += str.charAt(i) - '0';
+		}
+		System.out.println(sum);
+	}
+}
