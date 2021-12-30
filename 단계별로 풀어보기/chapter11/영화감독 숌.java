5b1df78cedd7ee8397c623e1650e7620245ff004
syoh98 <76934280+syoh98@users.noreply.github.com>
2021-12-30 16:00:45 +0900

    Create 영화감독 숌.java

---
+++ 단계별로 풀어보기/chapter11/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+		scanner.close();
+	}
+}
