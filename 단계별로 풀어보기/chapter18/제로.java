df62f579f1db61f61b96ea64a689bb1ae8f22ef4
syoh98 <76934280+syoh98@users.noreply.github.com>
2022-01-05 00:01:08 +0900

    Create 제로.java

---
+++ 단계별로 풀어보기/chapter18/제로.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+
+		int K = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 0; i < K; i++) {
+			int temp = Integer.parseInt(br.readLine());
+
+			if (temp == 0) {
+				list.remove(list.size() - 1);
+				//list.add(temp);
+			}
+			else {
+				list.add(temp);
+			}
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		
+		System.out.println(sum);
+	}
+}
