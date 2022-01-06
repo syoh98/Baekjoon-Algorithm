72458467581bc622f95c26b7a49b48c47b4b0ea9
syoh98 <76934280+syoh98@users.noreply.github.com>
2022-01-06 23:52:37 +0900

    8393

--- 단계별로 풀어보기/chapter3/합.java
+++ 단계별로 풀어보기/chapter3/합.java
@@ -4,17 +4,7 @@ public class Main {
 	@SuppressWarnings("null")
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
-		/*
-		 * int hour = in.nextInt(); int minute = in.nextInt(); int ahour = 0;
-		 */
 
-		/*
-		 * if (hour == 0) { ahour = 24 * 60; } else { ahour = hour * 60; }
-		 * 
-		 * int totalMinute = ahour + minute - 45; // 창영이가 원하는 알람 시간
-		 * 
-		 * System.out.println(totalMinute / 60 + " " + totalMinute % 60);
-		 */
 		int n = scanner.nextInt();
 		int sum = 0;
 		for (int i = 1; i <= n; i++) {
