302719f561d56a406f6fbc6241897fa042fea7d7
syoh98 <76934280+syoh98@users.noreply.github.com>
2022-01-06 23:55:30 +0900

    10869

--- 단계별로 풀어보기/chapter1/사칙연산.java
+++ 단계별로 풀어보기/chapter1/사칙연산.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String args[]) {
@@ -12,7 +12,5 @@ public class Main {
 		System.out.println(a * b);
 		System.out.println(a / b);
 		System.out.println(a % b);
-
-
 	}
 }
