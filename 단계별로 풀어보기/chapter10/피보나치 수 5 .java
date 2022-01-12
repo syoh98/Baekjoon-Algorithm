e0394d279637219ae86649af60f24107b1069d23
syoh98 <76934280+syoh98@users.noreply.github.com>
2022-01-12 15:19:59 +0900

    10870

---
+++ 단계별로 풀어보기/chapter10/피보나치 수 5 .java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765};
+		
+		System.out.println(arr[N]);
+		
+	}
+}
