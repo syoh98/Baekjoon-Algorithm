c2943397625ed3ee1bf7f7822a22ff2e67f93f1e
syoh98 <76934280+syoh98@users.noreply.github.com>
2022-02-04 18:46:49 +0900

    기능개발

---
+++ 프로그래머스/java/기능개발.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        int[] answer = new int[progresses.length];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // answer 배열에 각각 일의 수를 삽입
+        for (int i = 0; i < progresses.length; i++) {
+			      int cnt = 0;
+
+			      while (progresses[i] < 100) {
+				        progresses[i] += speeds[i];
+				        cnt++;
+			      }
+			      answer[i] = cnt;
+		    }
+
+        for (int i = 0; i < answer.length; i++) {
+			      int current = answer[i];
+			      int cnt = 1;
+
+            // 건너뛰도록
+			      if (current < 0) {
+				        continue;
+			      }
+
+			      for (int j = i + 1; j < answer.length; j++) {
+				        int next = answer[j];
+
+				        if (current >= next) {
+					          answer[j] = -1;
+					          cnt++;
+				        } else {
+					            break;
+				        }
+			      }
+			      list.add(cnt);	
+		   }
+        return list;
+    }
+}
